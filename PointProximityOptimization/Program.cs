bc72ed505782560e541e364a7624d81620143c60
KasperRAa <kasper.r.aagesen@gmail.com>
2026-04-20 17:35:23 +0200

    Parallel Search

--- PointProximityOptimization/Program.cs
+++ PointProximityOptimization/Program.cs
@@ -1,26 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Diagnostics;
 using System.Drawing;
 
 List<PointF> points = new List<PointF>();
 Size size = new Size(10, 10);
 const float radius = 1;
 
-const string midFix = " search result for highest proximity count: ";
-
 Random rng = new Random(0);
-
+Stopwatch setupSW = Stopwatch.StartNew();
 for (int i = 0; i < 10_000; i++)
 {
     double x = rng.NextDouble() * size.Width;
     double y = rng.NextDouble() * size.Height;
     points.Add(new PointF((float)x, (float)y));
 }
+setupSW.Stop();
+Console.WriteLine($"{points.Count} points in an area of {size.Width * size.Height} with dimensions [{size.Width};{size.Height}] ({setupSW.Elapsed.Milliseconds}ms)");
 
-Console.WriteLine($"{points.Count} points in an area of {size.Width * size.Height} with dimensions [{size.Width};{size.Height}]");
-BasicSearch(points, radius);
+Console.WriteLine($"Search with radius of {radius:0.00}:");
+List<Func<IReadOnlyList<PointF>, Size, float, int>> funcs = new() { BasicSearch, ParallelSearch };
+foreach (var func in funcs)
+{
+    string name = func.Method.Name;
+    Stopwatch sw = Stopwatch.StartNew();
+    int count = func(points, size, radius);
+    sw.Stop();
+    long time = sw.ElapsedMilliseconds;
+    Console.WriteLine($"\t{name} result for highest proximity count: {count} ({time}ms)");
+}
+Console.ReadKey(true);
 
-void BasicSearch(IReadOnlyList<PointF> points, float radius)
+int BasicSearch(IReadOnlyList<PointF> points, Size size, float radius)
 {
     int total = points.Count;
     int highestCount = 0;
@@ -30,9 +41,32 @@ void BasicSearch(IReadOnlyList<PointF> points, float radius)
         for (int j = 0; j < total; j++)
         {
             if (j == i) continue;
-            if ((points[i].ToVector2() - points[j].ToVector2()).Length() < radius) count++;
-            if (count > highestCount) highestCount = count;
+            if (GetDistance(points[i], points[j]) < radius) count++;
         }
+        if (count > highestCount) highestCount = count;
     }
-    Console.WriteLine($"Basic{midFix}{highestCount}");
+    return highestCount;
+}
+int ParallelSearch(IReadOnlyList<PointF> points, Size size, float radius)
+{
+    int total = points.Count;
+    int highestCount = 0;
+    Parallel.For(0, total, (i) =>
+    {
+        int count = 0;
+        for (int j = 0; j < total; j++)
+        {
+            if (j == i) continue;
+            if (GetDistance(points[i], points[j]) < radius) count++;
+        }
+        if (count > highestCount) highestCount = count;
+    });
+    return highestCount;
+}
+
+float GetDistance(PointF p1, PointF p2)
+{
+    float dx = p1.X - p2.X;
+    float dy = p1.Y - p2.Y;
+    return MathF.Sqrt(dx * dx + dy * dy);
 }
