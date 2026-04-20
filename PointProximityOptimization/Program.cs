01ddbdcd7e55e0abdac3bbd40f60beedeff933b1
KasperRAa <kasper.r.aagesen@gmail.com>
2026-04-20 19:14:57 +0200

    UI

--- PointProximityOptimization/Program.cs
+++ PointProximityOptimization/Program.cs
@@ -1,37 +1,58 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System;
 using System.Diagnostics;
 using System.Drawing;
 
-List<PointF> points = new List<PointF>();
 Size size = new Size(10, 10);
-const float radius = 1;
+List<Func<Size, IReadOnlyList<PointF>, float, int>> allFuncs = new() { BasicSearch, ParallelSearch, GridSearch, ParallelGridSearch };
 
-Random rng = new Random(0);
-Stopwatch setupSW = Stopwatch.StartNew();
-for (int i = 0; i < 10_000; i++)
+while (true)
 {
-    double x = rng.NextDouble() * size.Width;
-    double y = rng.NextDouble() * size.Height;
-    points.Add(new PointF((float)x, (float)y));
-}
-setupSW.Stop();
-Console.WriteLine($"{points.Count} points in an area of {size.Width * size.Height} with dimensions [{size.Width};{size.Height}] ({setupSW.Elapsed.Milliseconds}ms)");
+    List<int?> counts = new() { null, 10, 50, 100, 500, 1_000, 5_000, 10_000, 50_000, 100_000 };
+    int? count = PickFromOptions(counts, (i) => i.HasValue ? i.Value.ToString() : "Exit");
+    if (count is null) break;
+    Console.Clear();
+    List<PointF> points = GetPoints(count.Value);
+    while (true)
+    {
+        List<float?> radii = new() { null, 0.1f, 0.25f, 0.5f, 0.75f, 1f, 2f, 4f, 8f, 16f };
+        float? radius = PickFromOptions(radii, (i) => i.HasValue ? i.Value.ToString() : "Go back to Count");
+        if (radius is null) break;
+        Console.Clear();
+        while (true)
+        {
+            Console.WriteLine($"{points.Count} points in an area of {size.Width * size.Height} with dimensions [{size.Width};{size.Height}] and radius of {radius}");
+            List<Action<Size, IReadOnlyList<PointF>, float, List<Func<Size, IReadOnlyList<PointF>, float, int>>>?> displayOptions = new() { null, TryAllSearches, TrySpecificSearch };
+            var func = PickFromOptions(displayOptions, f => f is not null ? f.Method.Name[12..^4] : "Go back to Radius");
+            if (func is null) break;
+            Console.Clear();
 
-Console.WriteLine($"Search with radius of {radius:0.00}:");
-List<Func<IReadOnlyList<PointF>, Size, float, int>> funcs = new() { BasicSearch, ParallelSearch, GridSearch, ParallelGridSearch };
-foreach (var func in funcs)
+            func(size, points, radius.Value, allFuncs);
+            Console.ReadKey(true);
+            Console.Clear();
+        }
+    }
+}
+
+List<PointF> GetPoints(int total)
 {
-    string name = func.Method.Name[12..^4];
-    Stopwatch sw = Stopwatch.StartNew();
-    int count = func(points, size, radius);
-    sw.Stop();
-    long time = sw.ElapsedMilliseconds;
-    Console.WriteLine($"\t{name} result for highest proximity count with radius of {radius}: {count} ({time}ms)");
+    Random rng = new Random();
+    List<PointF> points = new();
+    Stopwatch setupSW = Stopwatch.StartNew();
+    for (int i = 0; i < total; i++)
+    {
+        double x = rng.NextDouble() * size.Width;
+        double y = rng.NextDouble() * size.Height;
+        points.Add(new PointF((float)x, (float)y));
+    }
+    setupSW.Stop();
+    Console.WriteLine($"{points.Count} points in an area of {size.Width * size.Height} with dimensions [{size.Width};{size.Height}] ({setupSW.Elapsed.Milliseconds}ms)");
+    return points;
 }
-Console.ReadKey(true);
 
-int BasicSearch(IReadOnlyList<PointF> points, Size size, float radius)
+
+int BasicSearch(Size size, IReadOnlyList<PointF> points, float radius)
 {
     int total = points.Count;
     int highestCount = 0;
@@ -47,7 +68,7 @@ int BasicSearch(IReadOnlyList<PointF> points, Size size, float radius)
     }
     return highestCount;
 }
-int ParallelSearch(IReadOnlyList<PointF> points, Size size, float radius)
+int ParallelSearch(Size size, IReadOnlyList<PointF> points, float radius)
 {
     int total = points.Count;
     int highestCount = 0;
@@ -63,7 +84,7 @@ int ParallelSearch(IReadOnlyList<PointF> points, Size size, float radius)
     });
     return highestCount;
 }
-int GridSearch(IReadOnlyList<PointF> points, Size size, float radius)
+int GridSearch(Size size, IReadOnlyList<PointF> points, float radius)
 {
     //Create Offsets
     List<Point> offsets = new() {
@@ -114,7 +135,7 @@ int GridSearch(IReadOnlyList<PointF> points, Size size, float radius)
     }
     return highestCount;
 }
-int ParallelGridSearch(IReadOnlyList<PointF> points, Size size, float radius)
+int ParallelGridSearch(Size size, IReadOnlyList<PointF> points, float radius)
 {
     //Create Offsets
     List<Point> offsets = new() {
@@ -172,3 +193,46 @@ float GetDistance(PointF p1, PointF p2)
     float dy = p1.Y - p2.Y;
     return MathF.Sqrt(dx * dx + dy * dy);
 }
+
+void TryAllSearches(Size size, IReadOnlyList<PointF> points, float radius, List<Func<Size, IReadOnlyList<PointF>, float, int>> funcs)
+{
+    Console.WriteLine($"{points.Count} points in an area of {size.Width * size.Height} with dimensions [{size.Width};{size.Height}] and radius of {radius:0.00}:");
+    foreach (var func in funcs)
+    {
+        Console.Write("\t");
+        TrySearch(size, points, radius, func);
+    }
+}
+void TrySpecificSearch(Size size, IReadOnlyList<PointF> points, float radius, List<Func<Size, IReadOnlyList<PointF>, float, int>> funcs)
+{
+    var func = PickFromOptions(funcs, GetNameOfFunc);
+    Console.WriteLine($"{points.Count} points in an area of {size.Width * size.Height} with dimensions [{size.Width};{size.Height}] and radius of {radius:0.00}:");
+    Console.Write("\t");
+    TrySearch(size, points, radius, func);
+}
+void TrySearch(Size size, IReadOnlyList<PointF> points, float radius, Func<Size, IReadOnlyList<PointF>, float, int> func)
+{
+    string name = GetNameOfFunc(func);
+    Stopwatch sw = Stopwatch.StartNew();
+    int count = func(size, points, radius);
+    sw.Stop();
+    long time = sw.ElapsedMilliseconds;
+    Console.WriteLine($"{name} result for highest proximity count with radius of {radius}: {count} ({time}ms)");
+}
+
+T? PickFromOptions<T>(List<T?> options, Func<T, string> getString)
+{
+    int count = options.Count;
+    if (count > 10) throw new ArgumentOutOfRangeException("\"options\" must be less than 10");
+    Console.WriteLine("Choose 1 of:");
+    char? c = null;
+    while (c is null || !char.IsDigit(c.Value) || int.Parse(c.Value.ToString()) >= count)
+    {
+        int n = 0;
+        foreach (var option in options) Console.WriteLine($"\t{n++}: {getString(option)}");
+        c = Console.ReadKey(true).KeyChar;
+    }
+    return options[int.Parse(c.Value.ToString())];
+}
+
+string GetNameOfFunc(Func<Size, IReadOnlyList<PointF>, float, int> func) => func.Method.Name[12..^4];
